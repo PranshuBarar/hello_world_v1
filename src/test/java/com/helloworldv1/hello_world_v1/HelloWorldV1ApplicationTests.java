0a9a39b0a1ec02536d1e6f8e5029ce141e1ed270
pranshubarar1 <pranshubarar1851996@gmail.com>
2024-11-05 13:14:24 +0530

    first commit

---
+++ src/test/java/com/helloworldv1/hello_world_v1/HelloWorldV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworldv1.hello_world_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
